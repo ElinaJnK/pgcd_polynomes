c981ff6b1fbbe5d42543775b26744d42179e57e3
ElinaJnK <jankovskajae@yahoo.com>
2023-01-27 16:17:34 +0100

    comments

--- tests/NTL/big_int.cpp
+++ tests/NTL/big_int.cpp
@@ -1,3 +1,10 @@
+/***
+ * To compile run 
+ * g++ -g -O2 -std=c++11 -pthread  big_int.cpp -o big_int -lntl -lgmp -lm
+ * If you are missing the gmp library: https://libntl.org/doc/tour-unix.html
+ * when you do make install (from here https://libntl.org/doc/tour-gmp.html) 
+ * do not forget to add 'sudo' before make.
+ * ***/
 #include <NTL/ZZ.h>
 
 using namespace std;
